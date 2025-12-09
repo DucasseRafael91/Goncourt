03193eb6232694038c9c4310e1032ea66bea8ec5
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-09 10:33:09 +0100

    Ajout d'attributs dans les classe author book et editor

--- goncourt/models/Author.py
+++ goncourt/models/Author.py
@@ -6,6 +6,7 @@ Classe Auteur représentant un auteur d'un livre
 
 from dataclasses import dataclass, field
 from typing import Optional
+from models import Book
 
 
 @dataclass
@@ -15,6 +16,13 @@ class Author:
     last_name: str
     first_name: str
     biographie: Optional[str] = field(default=None, init=False)
+    books_written: list[Book] = field(default_factory=list, init=False)
+
+    def add_book(self, book: Book) -> None:
+        """Ajoute un livre à la liste des livres édités."""
+        if book not in self.books_written:
+            self.books_written.append(book)
+        book.editor = self
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return f"{self.first_name} {self.last_name}"
--- goncourt/models/Book.py
+++ goncourt/models/Book.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-"""
-Classe Livre représentant un livre dans le contexte du prix Goncourt
-"""
-
 from dataclasses import dataclass, field
 from datetime import date
 from typing import Optional
 from models import Editor
+from models import Author
 
 
 @dataclass
@@ -19,21 +16,24 @@ class Book:
     publication_date: date
     pages: int
     editor_price: float
-    editor: int
-    author: int
+    editor: Editor
+    author: Author
 
     def set_editor(self, editor: Editor) -> None:
-        """Indique quel est l'éditeur de ce livre."""
-        if editor != self.editor:
-            # il y a quelque chose à faire
-            if self.editor is not None:
-                # un autre enseignant enseignait précédemment ce cours, qui ne doit
-                # donc plus faire partie de la liste des cours qu'il enseigne
-                editor.
-            # ajout du cours à l'enseignant indiqué
-            teacher.courses_teached.append(self)
-            # spécification de l'enseignant de ce cours
-            self.teacher = teacher
+        """Associe un éditeur à ce livre."""
+        if self.editor is not None and self in self.editor.books_edited:
+            self.editor.books_edited.remove(self)
+        self.editor = editor
+        if self not in editor.books_edited:
+            editor.books_edited.append(self)
+
+    def set_author(self, author: Author) -> None:
+        """Associe un éditeur à ce livre."""
+        if self.author is not None and self in self.author.books_written:
+            self.author.books_written.remove(self)
+        self.author = author
+        if self not in author.books_written:
+            author.books_written.append(self)
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return self.title
