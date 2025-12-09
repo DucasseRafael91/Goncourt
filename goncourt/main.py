396f35d4db84fcee6091c7485956ebb4bf0093e7
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-09 11:30:21 +0100

    Création d'une boucle pour selectionner son choix dans le fichier main.py

--- goncourt/main.py
+++ goncourt/main.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -20,7 +19,30 @@ Bienvenue dans le prix Goncourt
 
     goncourt: Goncourt = Goncourt()
 
-    print(goncourt.get_all_books())
+    while True:
+        print("\nVeuillez choisir une option :")
+        print("1 - Voir tous les livres sélectionnés au prix Goncourt")
+        print("2 - Indiquer les livres faisant partie de la deuxième selection")
+        print("3 - Indiquer les livres faisant partie de la troisième selection")
+        print("4 - Indiquer le nombre de votes obtenus par chaque livre présent au dernier tour de scrutin")
+        print("5 - Quitter")
+
+        choice = input("Tapez 1, 2 ou 3 : ")
+
+        if choice == "1":
+            print("\nListe des livres :")
+            print(goncourt.get_all_books())
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            print("Au revoir!")
+            break
+        else:
+            print("Choix invalide, veuillez taper 1, 2 ou 3.")
 
 
 if __name__ == '__main__':
